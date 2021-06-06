9cedd34e750cf57bdc8aad562c62a255fe518cef
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-06-06 20:24:22 +0300

    feat(main) added meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,45 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <div>
+        <meta name="keywords" content="fivem, gta5, gat 5, gta 5, gta five, five m, 5m, 5, m, 5m, grand theft auto 5, gta v, grand theft auto v, grand, theft, auto, esx, esx-reborn, notsiege, notsiege-development, not, siege, development, fivem script, fivem scripts, fivem cars, cars, fivem skins, fivem peds, eup, fivem eup"/>
+        <meta name="description" content="notsiege-development is a group of people devoted to making GTAV a better place thru mods. We mainly make cars, skins, scripts, maps but we also make much more mods! So come check us out"/>
+        <meta name="subject" content="GTAV Mods"/>
+        <meta name="copyright"content="notsiege-development"/>
+        <meta name="language" content="EN"/>
+        <meta name="robots" content="index,follow" />
+        <meta name="revised" content="Sunday, July 6th, 2021, 8:16 pm" />
+        <meta name="topic" content="GTA 5 Modding"/>
+        <meta name="summary" content="notsiege development is a group of people that make mods for FiveM and GTAV"/>
+        <meta name="Classification" content="Business"/>
+        <meta name="author" content="hamy & siege, pr@notsiege.dev"/>
+        <meta name="designer" content="SaltySea"/>
+        <meta name="reply-to" content="pr@notsiege.dev"/>
+        <meta name="owner" content="notsiege-development"/>
+        <meta name="url" content="https://notsiege.dev"/>
+        <meta name="identifier-URL" content="https://notsiege.dev"/>
+        <meta name="directory" content="submission"/>
+        <meta name="category" content="Gaming"/>
+        <meta name="coverage" content="Worldwide"/>
+        <meta name="distribution" content="Global"/>
+        <meta name="rating" content="General"/>
+        <meta name="revisit-after" content="7 days"/>
+        <meta http-equiv="Expires" content="0"/>
+        <meta name="og:title" content="notsiege development"/>
+        <meta name="og:type" content="Buisness"/>
+        <meta name="og:url" content="https://notsiege.dev"/>
+        <meta name="og:image" content="https://hamy.iodine.gg/346ea.png"/>
+        <meta name="og:site_name" content="notsiege-development"/>
+        <meta name="og:description" content="notsiege-development is a group of people devoted to making GTAV a better place thru mods. We mainly make cars, skins, scripts, maps but we also make much more mods! So come check us out! Make sure to check out iodine.gg aswell!"/>
+        <meta name="og:email" content="pr@notsiege.dev"/>
+        <meta name="og:region" content="CA"/>
+        <meta name="og:postal-code" content="00000"/>
+        <meta name="og:country-name" content="USA"/>
+        <meta property="og:video" content="https://streamable.com/ki4kkj" />
+        <meta property="og:video:height" content="640" />
+        <meta property="og:video:width" content="385" />
+        <meta property="og:video:type" content="application/x-shockwave-flash" />
+      </div>
     </Layout>
   )
 }
