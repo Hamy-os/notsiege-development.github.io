3fcfccff5a5848541d14fd92f74b0bbc5e7a9a66
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-06-07 15:05:39 +0300

    feat(main) Added a team page

--- pages/team.js
+++ pages/team.js
@@ -2,16 +2,28 @@ import styles from '../styles/Team.module.css'
 const  team = () => {
     return (
         <div>
-            <div className="team">
+            <div className={styles.team}>
                 <h1>Our Team</h1>
-                <p>Our team is composed of the following people</p> make a bot to select users with team role and display as li
+                <h2>Our team is composed of the following people</h2>
             </div>
-            <div>
-                <ul>
-                    <li className={styles.facebook}> <a>Laboris commodo mollit voluptate esse sint tempor ipsum.</a></li>
-                    <li className="facebook"> <a>Laboris commodo mollit voluptate esse sint tempor ipsum.</a></li>
-                    <li className="facebook"> <a>Laboris commodo mollit voluptate esse sint tempor ipsum.</a></li>
-                    <li className="facebook"> <a>Laboris commodo mollit voluptate esse sint tempor ipsum.</a></li>
+            <div className={styles.flexcontainer}>
+                <ul className={styles.wrapper}>
+                    <div>
+                        <img src="https://hamy.iodine.gg/c0d8i.webp" alt="image of notsiege"></img>
+                        <li className={styles.siege}> <a>Notsiege is the founder of NSD and is the reason all this exists</a></li>
+                    </div>
+                    <div>
+                        <img src="https://hamy.iodine.gg/7a7f6.webp" alt="image of notpulse"></img>
+                        <li className={styles.pulse}> <a>Notpulse is the co-founder of NSD and makes sure everything goes smoothly</a></li>
+                    </div>
+                    <div>
+                        <img src="https://hamy.iodine.gg/5e9i9.webp" alt="image of SaltySea"></img>
+                        <li className={styles.hamy}> <a>Hamy is the lead dev at NSD and oversees all operations</a></li>
+                    </div>
+                    <div>
+                        <img src="https://hamy.iodine.gg/8b749.webp" alt="image of Jacob"></img>
+                        <li className={styles.jacob}> <a>Jacob is a developer at NSD and made the beautiful Neon script</a></li>
+                    </div>
                 </ul>
             </div>
         </div>
