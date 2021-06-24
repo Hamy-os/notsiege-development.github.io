120a907e28bfb22a080fb37769f43843d23b0cf6
Hamy <64610533+Hamy-os@users.noreply.github.com>
2021-06-24 23:40:09 +0300

    feat(main) website rewrite in next.js
    so it can get merged to main :P

--- pages/jesusisaeggandsoishamylolwtfisthislink.js
+++
@@ -1,9 +0,0 @@
-const jesusisaeggandsoishamylolwtfisthislink = () => {
-    return (
-        <div>
-            Why are you here?
-        </div>
-    );
-}
- 
-export default jesusisaeggandsoishamylolwtfisthislink;
--- pages/team.js
+++ pages/team.js
@@ -1,10 +1,35 @@
+import styles from '../styles/team.module.css'
+
 const  team = () => {
     return (
         <div>
-            <div className="team">
-                <h1>Our Team</h1>
-                <p>Our team is composed by the following people</p> // make a bot to select users with team role and display as li
+            <body className={styles.body}>
+            <div className={styles.header}>
+                <h1>NotSiege development team</h1>
+                <p>Our team is composed of the following people.</p>
             </div>
+
+            <div className={styles.row}> 
+            <div className={styles.column}>
+                <img src="https://hamy.iodine.gg/i8b64.webp" style={{ width: '100%' }}/>
+                <p className={styles.names}>NotSiege | Owner</p>
+            </div>
+            <div className={styles.column}>
+                <img src="https://hamy.iodine.gg/gcbb0.webp" style={{ width: '100%' }}/>
+                <p className={styles.names}>NotPulse | Manager</p>
+            </div>  
+            <div className={styles.column}>
+                <img src="https://hamy.iodine.gg/0jcg5.webp" style={{ width: '100%' }}/>
+                <p className={styles.names}>SaltySea | Team Lead</p>
+            </div>
+            <div className={styles.column}>
+                <img src="https://hamy.iodine.gg/gcbb0.webp" style={{ width: '100%' }}/>
+                <p className={styles.names}>Jacob | Team</p>
+                <br></br>
+                <br></br>
+            </div>
+            </div>
+            </body>
         </div>
     );
 }
